e5b4f143f8205feccef34e8e3c8bed86f3402478
Anton Skorochod <anton@nsl.cz>
2019-08-25 11:17:36 +0200

    First commit :)

---
+++ README.md
@@ -0,0 +1,10 @@
+# Prusa ETA Override
+
+Prusa "NORMAL MODE: Percent done:" messages ETA override for OctoPi
+
+## Setup
+
+Install via the bundled [Plugin Manager](https://github.com/foosel/OctoPrint/wiki/Plugin:-Plugin-Manager)
+or manually using this URL:
+
+    https://github.com/kanocz/octopi_eta_override/archive/master.zip
---
+++ octoprint_PrusaETAOverride/__init__.py
@@ -0,0 +1,64 @@
+# coding=utf-8
+from __future__ import absolute_import
+import octoprint.plugin
+import re
+from octoprint.printer.estimation import PrintTimeEstimator
+
+eta = 0
+r = re.compile(
+    r'NORMAL MODE: Percent done: \d+; print time remaining in mins: (\d+)')
+
+
+def pETAeveryLine(comm, line, *args, **kwargs):
+    global eta, r
+    m = r.search(line)
+    if m:
+        eta = int(m.group(0)) * 60
+    return line
+
+
+class PrusaetaoverridePlugin(octoprint.plugin.OctoPrintPlugin):
+
+    def get_update_information(self):
+        return dict(
+            PrusaETAOverride=dict(
+                displayName="Prusa ETA override Plugin",
+                displayVersion=self._plugin_version,
+
+                # version check: github repository
+                type="github_release",
+                user="kanocz",
+                repo="octopi_eta_override",
+                current=self._plugin_version,
+
+                # update method: pip
+                pip="https://github.com/kanocz/octopi_eta_override/archive/{target_version}.zip"
+            )
+        )
+
+
+class pETAPrintTimeEstimator(PrintTimeEstimator):
+    def __init__(self, job_type):
+        super(pETAPrintTimeEstimator, self).__init__(job_type)
+
+    def estimate(self, progress, printTime, cleanedPrintTime, statisticalTotalPrintTime, statisticalTotalPrintTimeType):
+        return eta, "estimate"
+
+
+def pETAfactory(*args, **kwargs):
+    return pETAPrintTimeEstimator
+
+
+__plugin_name__ = "Prusa ETA override Plugin"
+
+
+def __plugin_load__():
+    global __plugin_implementation__
+    __plugin_implementation__ = PrusaetaoverridePlugin()
+
+    global __plugin_hooks__
+    __plugin_hooks__ = {
+        "octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information,
+        "octoprint.comm.protocol.gcode.received": pETAeveryLine,
+        "octoprint.printer.estimation.factory": pETAfactory
+    }
